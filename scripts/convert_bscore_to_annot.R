bed41cf318ca08ef05ae302bf0764d120078013f
Mariadaria Ianni-Ravn <mariadaria@gmail.com>
2023-04-25 15:23:06 -0500

    converting b scores

--- scripts/convert_bscore_to_annot.R
+++ scripts/convert_bscore_to_annot.R
@@ -6,17 +6,20 @@ library(tidyverse)
 library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
-chr <- args[1] %>% as.integer()
-ld_file <- args[2]
-dir.create(file.path("data/bscores/snpwise/"))
-output_file <- paste0("data/bscores/snpwise/snpwise.CADD.chr", chr, ".bmap.txt")
 
-bmap <- fread(paste0("data/bscores/CADD_bestfit/chr", chr, ".bmap.txt"))
+dir.create(file.path("data/bscores/snpwise/"))
+input_file <- args[1]
+ld_file <- args[2]
+output_file <- args[3]
+
+
+bmap <- fread(input_file)
 ldscore <- fread(ld_file)
 
 colnames(bmap) <- c("B","length")
 
-bmap <- bmap %>% mutate(bin_end=cumsum(length),bin_start=bin_end-length,B=B/1000,CHR=chr)
+bmap <- bmap %>%
+          mutate(bin_end=cumsum(length),bin_start=bin_end-length,B=B/1000,CHR=chr)
 
 # Define function to match SNP BP to bin in bmap
 match_snp_to_bin <- function(snp_bp, bmap) {
@@ -35,4 +38,4 @@ ldscore <- ldscore %>% filter(CHR==chr) %>%
 # Merge ldscore and bmap by bin
 merged <- ldscore %>% left_join(bmap,by=c("bin_end","CHR"))
 
-merged %>% select(CHR,SNP,BP,L2,B) %>% write_tsv(file=output_file)
+merged %>% select(CHR,SNP,BP,B) %>% write_tsv(file=output_file)
